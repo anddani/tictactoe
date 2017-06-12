6b4bb17dd29eca86f185c6b7d54329adab90dccc
anddani <anddani@kth.se>
2017-06-12 21:59:12 +0200

    Possible to analyze game state

--- ticTacToeClient/app/src/main/java/com/example/algo/tictactoe/GameState.kt
+++ ticTacToeClient/app/src/main/java/com/example/algo/tictactoe/GameState.kt
@@ -2,7 +2,7 @@ package com.example.algo.tictactoe
 
 import android.util.Log
 
-class GameState (val board : List<String>, val log : String) {
+class GameState (val board: List<String>, val log: String) {
 
     val playerSymbol = "X"
 
@@ -11,12 +11,65 @@ class GameState (val board : List<String>, val log : String) {
     }
 
     fun makeMove(move: Int) : List<String> =
-        board.take(move) + playerSymbol + board.drop(move+1)
+            board.take(move) + playerSymbol + board.drop(move+1)
 
     fun validMove(move: Int) : Boolean =
-        move != -1 && "V".equals(board[move])
+            move != -1 && "V".equals(board[move])
+
+    fun gameOver() : Boolean =
+            maxCons('X') == 3 || maxCons('O') == 3 || board.indexOf("V") == -1
+
+    fun getWinner() : String =
+        when {
+            maxCons('X') == 3 -> "X"
+            maxCons('O') == 3 -> "O"
+            board.indexOf("V") == -1 -> "V"
+            else -> ""
+        }
+
+    fun maxCons(player: Char) : Int {
+        val row : Int? = splitInThree(board)
+                .map { numCons(player, it.joinToString("")) }
+                .max()
+        val col : Int? = splitInThree(transpose(board))
+                .map { numCons(player, it.joinToString("")) }
+                .max()
+        val dia : Int? = maxOf(numCons(player, listOf(board[0], board[4], board[8]).joinToString("")),
+                               numCons(player, listOf(board[2], board[4], board[6]).joinToString("")))
+
+        return maxOf(row!!, col!!, dia!!)
+    }
+
+    fun transpose(b: List<String>) =
+            b.withIndex()
+                    .groupBy { it.index % 3 }
+                    .map { it.value.map { it.value } }
+                    .flatten()
+
+    fun splitInThree(b: List<String>) =
+            b.withIndex()
+                    .groupBy { it.index / 3 }
+                    .map { it.value.map { it.value } }
+
+    fun numCons(player: Char, row: String) : Int {
+        var cons = 0
+        var maxCons = 0
+        row.forEach {
+            if (it == player) {
+                cons++
+            }
+            else {
+                maxCons = maxOf(maxCons, cons)
+                cons = 0
+            }
+        }
+        return maxOf(maxCons, cons)
+    }
 
     override fun toString() : String {
-        return ""
+        return board.withIndex()
+                .groupBy { it.index / 3 }
+                .map { it.value.map { it.value } }
+                .toString()
     }
 }
--- ticTacToeClient/app/src/test/java/com/example/algo/tictactoe/GameStateTest.kt
+++ ticTacToeClient/app/src/test/java/com/example/algo/tictactoe/GameStateTest.kt
@@ -4,11 +4,18 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-
 class GameStateTest {
     val gameState = GameState(listOf("X", "O", "V",
                                      "X", "V", "O",
                                      "V", "V", "V"),  "")
+
+    val gameOverState1 = GameState(listOf("X", "O", "V",
+                                          "X", "X", "O",
+                                          "V", "V", "X"),  "")
+
+    val gameOverState2 = GameState(listOf("X", "O", "X",
+                                          "X", "X", "O",
+                                          "O", "X", "O"),  "")
     @Test
     fun validMove_isCorrect() {
         assertEquals(true,  gameState.validMove(2))
@@ -31,19 +38,36 @@ class GameStateTest {
 
     @Test
     fun gameStateToString_isCorrect() {
-        // TODO
-        assertEquals(true, false)
+        assertEquals("[[X, O, V], [X, V, O], [V, V, V]]",
+                gameState.toString())
     }
 
     @Test
     fun gameOver_isCorrect() {
-        // TODO
-        assertEquals(true, false)
+        assertEquals(false, gameState.gameOver())
+        assertEquals(true, gameOverState1.gameOver())
+        assertEquals(true, gameOverState2.gameOver())
+    }
+
+    @Test
+    fun numCons_isCorrect() {
+        assertEquals(2, gameState.maxCons('X'))
+        assertEquals(1, gameState.maxCons('O'))
+        assertEquals(3, gameOverState1.maxCons('X'))
+    }
+
+    @Test
+    fun transpose_isCorrect() {
+        assertEquals(listOf("X", "X", "V",
+                            "O", "V", "V",
+                            "V", "O", "V" ),
+                gameState.transpose(gameState.board))
     }
 
     @Test
     fun winner_isCorrect() {
-        // TODO
-        assertEquals(true, false)
+        assertEquals("", gameState.getWinner())
+        assertEquals("X", gameOverState1.getWinner())
+        assertEquals("V", gameOverState2.getWinner())
     }
 }
